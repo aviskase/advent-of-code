66622ed6cb856d8298843636a1353811a8282b01
Yuliya Bagriy <aviskase@gmail.com>
2019-12-23 21:41:25 -0500

    solved part 1

--- day22/puzzle.py
+++ day22/puzzle.py
@@ -1,6 +1,36 @@
+from typing import List
+
+
+def shuffle_deck(deck, instructions):
+    new_deck = deck
+    l = len(deck)
+    for instruction in instructions:
+        if instruction.startswith('deal into new stack'):
+            new_deck.reverse()
+        elif instruction.startswith('cut'):
+            size = int(instruction.split()[-1])
+            new_deck = new_deck[size:] + new_deck[:size]
+        elif instruction.startswith('deal with increment'):
+            size = int(instruction.split()[-1])
+            temp = [-1] * l
+            i = 0
+            for d in new_deck:
+                temp[i*size % l] = d
+                i += 1
+            new_deck = temp
+    return new_deck
+
+
+def part_one(lines: List[str]):
+    deck_size = 10007
+    deck = shuffle_deck(list(range(deck_size)), lines)
+    print('Part 1:', deck.index(2019))
+
+
 def solver():
     with open('input.txt', 'r') as f:
-        line = f.readline()
+        lines = f.readlines()
+        part_one(lines)
 
 
 if __name__ == '__main__':
---
+++ day22/test_puzzle.py
@@ -0,0 +1,20 @@
+import pytest
+
+from day22.puzzle import shuffle_deck
+
+
+@pytest.mark.parametrize('size, instructions, result', [
+    (5, ['deal into new stack'], [4, 3, 2, 1, 0]),
+    (5, ['cut 1'], [1, 2, 3, 4, 0]),
+    (5, ['cut 3'], [3, 4, 0, 1, 2]),
+    (5, ['cut -1'], [4, 0, 1, 2, 3]),
+    (5, ['cut -3'], [2, 3, 4, 0, 1]),
+    (10, ['deal with increment 3'], [0, 7, 4, 1, 8, 5, 2, 9, 6, 3]),
+    (10, 'deal with increment 7\ndeal into new stack\ndeal into new stack'.split('\n'), [0, 3, 6, 9, 2, 5, 8, 1, 4, 7]),
+    (10, 'cut 6\ndeal with increment 7\ndeal into new stack'.split('\n'), [3, 0, 7, 4, 1, 8, 5, 2, 9, 6]),
+    (10, 'deal with increment 7\ndeal with increment 9\ncut -2'.split('\n'), [6, 3, 0, 7, 4, 1, 8, 5, 2, 9]),
+    (10, 'deal into new stack\ncut -2\ndeal with increment 7\ncut 8\ncut -4\ndeal with increment 7\ncut 3\n'
+         'deal with increment 9\ndeal with increment 3\ncut -1'.split('\n'), [9, 2, 5, 8, 1, 4, 7, 0, 3, 6]),
+])
+def test_shuffle(size, instructions, result):
+    assert shuffle_deck(list(range(size)), instructions) == result
