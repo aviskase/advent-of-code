f2b67d85b2ee74f7697829d883b6764ab36f755b
Yuliya Bagriy <aviskase@gmail.com>
2020-12-01 20:55:59 -0500

    refac: day01

--- 2020/day01/puzzle.py
+++ 2020/day01/puzzle.py
@@ -4,7 +4,7 @@ from typing import List, Tuple
 def find_2020(expenses: List[int]) -> Tuple[int, int]:
     sorted_expenses = sorted(expenses)
     for first, a in enumerate(sorted_expenses[:-1]):
-        for second, b in enumerate(sorted_expenses[first+1:]):
+        for b in sorted_expenses[first+1:]:
             if a + b == 2020:
                 return a, b
 
@@ -13,7 +13,7 @@ def find3_2020(expenses: List[int]) -> Tuple[int, int, int]:
     sorted_expenses = sorted(expenses)
     for first, a in enumerate(sorted_expenses[:-2]):
         for second, b in enumerate(sorted_expenses[first+1:]):
-            for third, c in enumerate(sorted_expenses[second+2:]):
+            for c in sorted_expenses[second+2:]:
                 if a + b + c == 2020:
                     return a, b, c
 
