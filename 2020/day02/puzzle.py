da173fc5bad69263d42dd398d742ab697eecc94e
Yuliya Bagriy <aviskase@gmail.com>
2020-12-02 04:53:34 -0500

    feat: day02 part2

--- 2020/day02/puzzle.py
+++ 2020/day02/puzzle.py
@@ -11,11 +11,21 @@ def valid(rules: List[List[str]]) -> List[str]:
     return valid_passwords
 
 
+def valid2(rules: List[List[str]]) -> List[str]:
+    valid_passwords = []
+    for [rule, password] in rules:
+        numbers, letter = rule.split(' ')
+        a, b = map(lambda x: int(x) - 1, numbers.split('-'))
+        if (password[a] == letter and password[b] != letter) or (password[a] != letter and password[b] == letter):
+            valid_passwords.append(password)
+    return valid_passwords
+
+
 def solver():
     with open('input.txt', 'r') as f:
         rules = [line.split(': ') for line in f.readlines()]
         print('Part 1:', len(valid(rules)))
-        # print('Part 2:', )
+        print('Part 1:', len(valid2(rules)))
 
 
 if __name__ == '__main__':
--- 2020/day02/test_puzzle.py
+++ 2020/day02/test_puzzle.py
@@ -1,4 +1,4 @@
-from .puzzle import valid
+from .puzzle import valid, valid2
 
 data = [
     ['1-3 a', 'abcde'],
@@ -10,3 +10,5 @@ data = [
 def test_valid():
     assert {'abcde', 'ccccccccc'} == set(valid(data))
 
+def test_valid2():
+    assert {'abcde'} == set(valid2(data))
