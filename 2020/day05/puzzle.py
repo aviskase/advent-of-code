98ff31dce4620a6292fc4449b4bd3aa0ff345e36
Yuliya Bagriy <aviskase@gmail.com>
2020-12-05 14:50:29 -0500

    refac: fit for part 02

--- 2020/day05/puzzle.py
+++ 2020/day05/puzzle.py
@@ -16,19 +16,17 @@ def find_column(boarding_pass):
     return find_place(boarding_pass[7:], 'R', 7)
 
 
-def find_highest_seat_id(boarding_passes):
-    max_id = 0
-    for boarding_pass in boarding_passes:
-        seat_id = find_row(boarding_pass) * 8 + find_column(boarding_pass)
-        if seat_id > max_id:
-            max_id = seat_id
-    return max_id
+def all_ids(boarding_passes):
+    return sorted([
+        find_row(boarding_pass) * 8 + find_column(boarding_pass)
+        for boarding_pass in boarding_passes
+    ])
 
 
 def solver():
     with open('input.txt', 'r') as f:
         boarding_passes = f.read().strip().splitlines()
-        print('Part 1:', find_highest_seat_id(boarding_passes))
+        print('Part 1:', all_ids(boarding_passes)[-1])  # 842
         # print('Part 2:', )
 
 
