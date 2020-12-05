869dfe9a57194e1a5ab7cb1895d307016748dfe1
Yuliya Bagriy <aviskase@gmail.com>
2020-12-05 15:12:58 -0500

    feat: day05 part2

--- 2020/day05/puzzle.py
+++ 2020/day05/puzzle.py
@@ -1,3 +1,6 @@
+from more_itertools import pairwise, first_true
+
+
 def find_place(placepath, bottom_label, max_place):
     place = range(0, max_place+1)
     for p in placepath:
@@ -16,18 +19,15 @@ def find_column(boarding_pass):
     return find_place(boarding_pass[7:], 'R', 7)
 
 
-def all_ids(boarding_passes):
-    return sorted([
-        find_row(boarding_pass) * 8 + find_column(boarding_pass)
-        for boarding_pass in boarding_passes
-    ])
-
-
 def solver():
     with open('input.txt', 'r') as f:
         boarding_passes = f.read().strip().splitlines()
-        print('Part 1:', all_ids(boarding_passes)[-1])  # 842
-        # print('Part 2:', )
+        seat_ids = sorted([
+            find_row(boarding_pass) * 8 + find_column(boarding_pass)
+            for boarding_pass in boarding_passes
+        ])
+        print('Part 1:', seat_ids[-1])  # 842
+        print('Part 2:', first_true(pairwise(seat_ids), pred=lambda pair: pair[0] + 2 == pair[1])[0] + 1)  # 617
 
 
 if __name__ == '__main__':
