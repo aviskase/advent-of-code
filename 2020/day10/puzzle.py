b6072a834edce2cfecb540663b85cb8f19efa023
Yuliya Bagriy <aviskase@gmail.com>
2020-12-10 13:32:17 -0500

    feat: day10 part2

--- 2020/day10/puzzle.py
+++ 2020/day10/puzzle.py
@@ -1,3 +1,4 @@
+import functools
 from collections import Counter
 from itertools import starmap
 from typing import List
@@ -12,12 +13,37 @@ def find_differences(adapters: List[int]) -> Counter:
     return diffs
 
 
+def count_arrangements(adapters: List[int]) -> int:
+    all_adapters = [0] + sorted(adapters)
+    all_adapters.append(all_adapters[-1] + 3)
+    return traverse_arrangements(all_adapters)
+
+
+def traverse_arrangements(adapters: List[int]):
+    def valid_inc(pos, inc): return pos + inc < len(adapters) and adapters[pos+inc] - adapters[pos] <= 3
+
+    @functools.lru_cache()
+    def _traverse(pos):
+        if adapters[pos] == adapters[-1]:
+            return 1
+        counter = 0
+        if valid_inc(pos, 1):
+            counter += _traverse(pos+1)
+        if valid_inc(pos, 2):
+            counter += _traverse(pos+2)
+        if valid_inc(pos, 3):
+            counter += _traverse(pos+3)
+        return counter
+
+    return _traverse(0)
+
+
 def solver():
     with open('input.txt', 'r') as f:
         adapters = [int(x) for x in f.read().strip().splitlines()]
         diff = find_differences(adapters)
         print('Part 1:', diff.get(1) * diff.get(3))  # 2760
-        # print('Part 2:', )  #
+        print('Part 2:', count_arrangements(adapters))  # 13816758796288
 
 
 if __name__ == '__main__':
--- 2020/day10/test_puzzle.py
+++ 2020/day10/test_puzzle.py
@@ -1,6 +1,8 @@
 import pytest
 
-from .puzzle import find_differences
+from .puzzle import find_differences, count_arrangements
+
+my_data = [1, 4, 5]
 
 data = [int(x) for x in '''16
 10
@@ -54,3 +56,8 @@ def test_find_differences(d, expected1, expected3):
     diff = find_differences(d)
     assert diff.get(1) == expected1
     assert diff.get(3) == expected3
+
+
+@pytest.mark.parametrize('d, expected', [(my_data, 1), (data, 8), (second_data, 19208)])
+def test_count_arrangements(d, expected):
+    assert count_arrangements(d) == expected
