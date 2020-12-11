56cfc788b2b581d993b886e46ae2adb0d56f2711
Yuliya Bagriy <aviskase@gmail.com>
2020-12-11 15:04:11 -0500

    feat: day11 part2

--- 2020/day11/puzzle.py
+++ 2020/day11/puzzle.py
@@ -2,7 +2,7 @@ from copy import deepcopy
 from itertools import product
 from typing import List
 
-from more_itertools import flatten
+from more_itertools import flatten, first_true, quantify
 
 FLOOR = '.'
 OCCUPIED = '#'
@@ -21,28 +21,49 @@ def remove_border(seats: TSeats) -> TSeats:
     return [s[1:-1] for s in seats[1:-1]]
 
 
-def simulate(seats: TSeats, max_rounds=None) -> TSeats:
+def direct_neighbours(seats, i, j):
+    return [
+        seats[i-1][j-1], seats[i-1][j], seats[i-1][j+1],
+        seats[i][j-1], seats[i][j+1],
+        seats[i+1][j-1], seats[i+1][j], seats[i+1][j+1],
+    ]
+
+
+def all_visible(seats, i, j):
+    def not_floor(x): return x != FLOOR
+    max_rows = len(seats)
+    max_cols = len(seats[0])
+    return [
+        first_true(seats[i][j-1::-1], FLOOR, not_floor),  # ←
+        first_true(seats[i][j+1:], FLOOR, not_floor),  # →
+        first_true((seats[k][j] for k in range(i-1, -1, -1)), FLOOR, not_floor),  # ↑
+        first_true((seats[k][j] for k in range(i+1, max_rows)), FLOOR, not_floor),  # ↓
+
+        first_true((seats[k][l] for k, l in zip(range(i-1, -1, -1), range(j-1, -1, -1))), FLOOR, not_floor),  # ↖
+        first_true((seats[k][l] for k, l in zip(range(i+1, max_rows), range(j-1, -1, -1))), FLOOR, not_floor),  # ↙
+        first_true((seats[k][l] for k, l in zip(range(i-1, -1, -1), range(j+1, max_cols))), FLOOR, not_floor),  # ↗
+        first_true((seats[k][l] for k, l in zip(range(i+1, max_rows), range(j+1, max_cols))), FLOOR, not_floor),  # ↘
+    ]
+
+
+def simulate(seats: TSeats, max_rounds=None, occupied_threshold=4, neighbours_fn=direct_neighbours) -> TSeats:
     cur_round = 0
-    change_count = 999
+    with_changes = True
     cur_seats = add_border(seats)
     max_rows = len(cur_seats)-1
-    max_seats = len(cur_seats[0])-1
-    while cur_round != max_rounds and change_count > 0:
-        change_count = 0
+    max_cols = len(cur_seats[0])-1
+    while cur_round != max_rounds and with_changes:
+        with_changes = False
         next_seats = deepcopy(cur_seats)
-        for i, j in product(range(1, max_rows), range(1, max_seats)):
+        for i, j in product(range(1, max_rows), range(1, max_cols)):
             seat = cur_seats[i][j]
-            neighbours = [
-                cur_seats[i-1][j-1], cur_seats[i-1][j], cur_seats[i-1][j+1],
-                cur_seats[i][j-1], cur_seats[i][j+1],
-                cur_seats[i+1][j-1], cur_seats[i+1][j], cur_seats[i+1][j+1],
-            ]
+            neighbours = neighbours_fn(cur_seats, i, j)
             if seat == EMPTY and neighbours.count(OCCUPIED) == 0:
                 next_seats[i][j] = OCCUPIED
-                change_count += 1
-            elif seat == OCCUPIED and neighbours.count(OCCUPIED) >= 4:
+                with_changes = True
+            elif seat == OCCUPIED and neighbours.count(OCCUPIED) >= occupied_threshold:
                 next_seats[i][j] = EMPTY
-                change_count += 1
+                with_changes = True
         cur_round += 1
         cur_seats = next_seats
     return remove_border(cur_seats)
@@ -52,8 +73,9 @@ def solver():
     with open('input.txt', 'r') as f:
         seats = [list(x) for x in f.read().strip().splitlines()]
         end_state = simulate(seats)
-        print('Part 1:', list(flatten(end_state)).count(OCCUPIED))  # 2277
-        # print('Part 2:', )  #
+        print('Part 1:', quantify(flatten(end_state), lambda x: x == OCCUPIED))  # 2277
+        end_state = simulate(seats, occupied_threshold=5, neighbours_fn=all_visible)
+        print('Part 1:', quantify(flatten(end_state), lambda x: x == OCCUPIED))  # 2066
 
 
 if __name__ == '__main__':
--- 2020/day11/test_puzzle.py
+++ 2020/day11/test_puzzle.py
@@ -1,6 +1,6 @@
 import pytest
 
-from .puzzle import simulate
+from .puzzle import simulate, all_visible
 
 data = [list(d) for d in '''L.LL.LL.LL
 LLLLLLL.LL
@@ -54,3 +54,45 @@ L.#.L..#..
 @pytest.mark.parametrize('rounds, expected', [(1, one_round), (2, second_round), (None, end_round)])
 def test_simulate(rounds, expected):
     assert simulate(data, max_rounds=rounds) == expected
+
+
+one_round2 = [list(d) for d in '''#.##.##.##
+#######.##
+#.#.#..#..
+####.##.##
+#.##.##.##
+#.#####.##
+..#.#.....
+##########
+#.######.#
+#.#####.##
+'''.strip().splitlines()]
+
+second_round2 = [list(d) for d in '''#.LL.LL.L#
+#LLLLLL.LL
+L.L.L..L..
+LLLL.LL.LL
+L.LL.LL.LL
+L.LLLLL.LL
+..L.L.....
+LLLLLLLLL#
+#.LLLLLL.L
+#.LLLLL.L#
+'''.strip().splitlines()]
+
+end_round2 = [list(d) for d in '''#.L#.L#.L#
+#LLLLLL.LL
+L.L.L..#..
+##L#.#L.L#
+L.L#.LL.L#
+#.LLLL#.LL
+..#.L.....
+LLL###LLL#
+#.LLLLL#.L
+#.L#LL#.L#
+'''.strip().splitlines()]
+
+
+@pytest.mark.parametrize('rounds, expected', [(1, one_round2), (2, second_round2), (None, end_round2)])
+def test_simulate2(rounds, expected):
+    assert simulate(data, max_rounds=rounds, occupied_threshold=5, neighbours_fn=all_visible) == expected
