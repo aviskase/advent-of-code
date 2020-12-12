f44af47e1caa22a68348a47f65aab32441846f5c
Yuliya Bagriy <aviskase@gmail.com>
2020-12-12 15:14:59 -0500

    feat: day12 part2

--- 2020/day12/puzzle.py
+++ 2020/day12/puzzle.py
@@ -31,6 +31,33 @@ def move_ship(raw_commands: List[str]) -> (int, int):
     return x, y
 
 
+def move_ship_with_waypoint(raw_commands: List[str]) -> (int, int):
+    x = y = 0
+    xw = 10
+    yw = 1
+    for raw_cmd in raw_commands:
+        cmd = raw_cmd[0]
+        value = int(raw_cmd[1:])
+        if cmd == 'N':
+            yw += value
+        elif cmd == 'S':
+            yw -= value
+        elif cmd == 'E':
+            xw += value
+        elif cmd == 'W':
+            xw -= value
+        elif cmd == 'F':
+            x += (value * xw)
+            y += (value * yw)
+        elif (value == 270 and cmd == 'L') or (value == 90 and cmd == 'R'):
+            xw, yw = yw, -xw
+        elif (value == 90 and cmd == 'L') or (value == 270 and cmd == 'R'):
+            xw, yw = -yw, xw
+        else:
+            xw, yw = -xw, -yw
+    return x, y
+
+
 def mdistance(x: int, y: int) -> int:
     return abs(x) + abs(y)
 
@@ -39,7 +66,7 @@ def solver():
     with open('input.txt', 'r') as f:
         raw_commands = f.read().strip().splitlines()
         print('Part 1:', mdistance(*move_ship(raw_commands)))  # 1589
-        # print('Part 2:', )  #
+        print('Part 2:', mdistance(*move_ship_with_waypoint(raw_commands)))  # 23960
 
 
 if __name__ == '__main__':
--- 2020/day12/test_puzzle.py
+++ 2020/day12/test_puzzle.py
@@ -1,4 +1,4 @@
-from .puzzle import move_ship, mdistance
+from .puzzle import move_ship, mdistance, move_ship_with_waypoint
 
 data = '''F10
 N3
@@ -12,3 +12,9 @@ def test_move_ship():
     position = move_ship(data)
     assert position == (17, -8)
     assert mdistance(*position) == 25
+
+
+def test_move_ship_with_waypoint():
+    position = move_ship_with_waypoint(data)
+    assert position == (214, -72)
+    assert mdistance(*position) == 286
