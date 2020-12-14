d1b70d80406818cfcb38e54f11a771097577710d
Yuliya Bagriy <aviskase@gmail.com>
2020-12-14 18:43:42 -0500

    feat: day13 part2

--- 2020/day13/puzzle.py
+++ 2020/day13/puzzle.py
@@ -1,3 +1,4 @@
+import math
 from operator import mul
 from typing import List
 
@@ -11,12 +12,33 @@ def find_bus(timestamp: int, all_buses: List[str]) -> (int, int):
     return bus, arrival - timestamp
 
 
+def find_timestamp(all_buses: List[str]) -> int:
+    buses = [int(b) for b in all_buses if b != 'x']
+    remainders = [int(b)-i for i, b in enumerate(all_buses) if b != 'x']
+    total_prod = math.prod(buses)
+    prod = [total_prod // b for b in buses]
+    total = sum(r * p * inv(p, b) for r, p, b in zip(remainders, prod, buses))
+    return total % total_prod
+
+
+def inv(a, b):
+    # a * x = 1 (mod b)
+    # simplify
+    simplified_a = a % b
+    # aa * x = 1 (mod b)
+    x = 1
+    while True:
+        if (simplified_a * x) % b == 1:
+            return x
+        x += 1
+
+
 def solver():
     with open('input.txt', 'r') as f:
         timestamp = int(f.readline().strip())
         buses = f.readline().strip().split(',')
         print('Part 1:', mul(*find_bus(timestamp, buses)))  # 2845
-        # print('Part 2:', )  #
+        print('Part 2:', find_timestamp(buses))  # 487905974205117
 
 
 if __name__ == '__main__':
--- 2020/day13/test_puzzle.py
+++ 2020/day13/test_puzzle.py
@@ -1,5 +1,6 @@
-from .puzzle import find_bus
+import pytest
 
+from .puzzle import find_bus, find_timestamp
 
 timestamp = 939
 buses = '7,13,x,x,59,x,31,19'.split(',')
@@ -7,3 +8,15 @@ buses = '7,13,x,x,59,x,31,19'.split(',')
 
 def test_find_bus():
     assert find_bus(timestamp, buses) == (59, 5)
+
+
+@pytest.mark.parametrize('b, expected', [
+    (buses, 1068781),
+    ('17,x,13,19'.split(','), 3417),
+    ('67,7,59,61'.split(','), 754018),
+    ('67,x,7,59,61'.split(','), 779210),
+    ('67,7,x,59,61'.split(','), 1261476),
+    ('1789,37,47,1889'.split(','), 1202161486),
+])
+def test_find_timestamp(b, expected):
+    assert find_timestamp(b) == expected
