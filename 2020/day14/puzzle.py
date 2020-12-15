ccb07cd7ed841bbcf6435546852dd0988d86907f
Yuliya Bagriy <aviskase@gmail.com>
2020-12-14 20:15:19 -0500

    feat: day14 part2

--- 2020/day14/puzzle.py
+++ 2020/day14/puzzle.py
@@ -1,3 +1,4 @@
+from itertools import product
 from typing import Dict, List
 
 TMemory = Dict[int, int]
@@ -19,6 +20,30 @@ def execute_program(program: List[str]) -> TMemory:
     return memory
 
 
+def execute_program2(program: List[str]) -> TMemory:
+    memory = {}
+    mask_ones = None
+    mask_floating_positions = None
+    mask_floating = None
+    for cmd in program:
+        if cmd.startswith('mask = '):
+            mask = list(reversed(cmd.replace('mask = ', '')))
+            mask_ones = [i for i, m in enumerate(mask) if m == '1']
+            mask_floating_positions = [i for i, m in enumerate(mask) if m == 'X']
+            mask_floating = list(product([0, 1], repeat=mask.count('X')))
+        else:
+            addr, value = cmd.split('] = ')
+            addr = int(addr.replace('mem[', ''))
+            for i in mask_ones:
+                addr = set_bit(addr, i, 1)
+            for cur_mask in mask_floating:
+                floating_addr = addr
+                for p, m in zip(mask_floating_positions, cur_mask):
+                    floating_addr = set_bit(floating_addr, p, m)
+                memory[floating_addr] = int(value)
+    return memory
+
+
 def set_bit(num, offset, bit_value):
     if bit_value == 1:
         mask = 1 << offset
@@ -35,7 +60,7 @@ def solver():
     with open('input.txt', 'r') as f:
         raw_program = f.read().strip().splitlines()
         print('Part 1:', memory_sum(execute_program(raw_program)))  # 13496669152158
-        # print('Part 2:', )  #
+        print('Part 2:', memory_sum(execute_program2(raw_program)))  # 3278997609887
 
 
 if __name__ == '__main__':
--- 2020/day14/test_puzzle.py
+++ 2020/day14/test_puzzle.py
@@ -1,4 +1,4 @@
-from .puzzle import execute_program, memory_sum
+from .puzzle import execute_program, memory_sum, execute_program2
 
 data = '''mask = XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X
 mem[8] = 11
@@ -7,7 +7,18 @@ mem[8] = 0
 '''.strip().splitlines()
 
 
+data2 = '''mask = 000000000000000000000000000000X1001X
+mem[42] = 100
+mask = 00000000000000000000000000000000X0XX
+mem[26] = 1
+'''.strip().splitlines()
+
+
 def test_execute():
     memory = execute_program(data)
     assert set(memory.values()) == {101, 64}
     assert memory_sum(memory) == 165
+
+
+def test_execute2():
+    assert memory_sum(execute_program2(data2)) == 208
