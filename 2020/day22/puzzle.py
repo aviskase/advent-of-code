23b3e9fead2c8b895afe8c91fd772181a8e47400
Yuliya Bagriy <aviskase@gmail.com>
2020-12-22 10:51:07 -0500

    feat: day22 part 1

---
+++ 2020/day22/puzzle.py
@@ -0,0 +1,44 @@
+from collections import deque
+from typing import List
+
+THand = List[int]
+
+
+def parse(raw_data: str) -> (THand, THand):
+    player_1, player_2 = raw_data.split('\n\n', 2)
+    player_1 = [int(card) for card in player_1.strip().splitlines()[1:]]
+    player_2 = [int(card) for card in player_2.strip().splitlines()[1:]]
+    return player_1, player_2
+
+
+def play_drunkard(player_1: THand, player_2: THand) -> (THand, int):
+    cur_1 = deque(player_1)
+    cur_2 = deque(player_2)
+    while len(cur_1) and len(cur_2):
+        card_1 = cur_1.popleft()
+        card_2 = cur_2.popleft()
+        if card_1 > card_2:
+            cur_1.append(card_1)
+            cur_1.append(card_2)
+        else:
+            cur_2.append(card_2)
+            cur_2.append(card_1)
+    if len(cur_1):
+        return list(cur_1), 1
+    return list(cur_2), 2
+
+
+def score(player: THand) -> int:
+    return sum(i * c for i, c in enumerate(reversed(player), 1))
+
+
+def solver():
+    with open('input.txt', 'r') as f:
+        p1, p2 = parse(f.read().strip())
+        winner_hand, winner_id = play_drunkard(p1, p2)
+        print('Part 1:', score(winner_hand))  # 33403
+        # print('Part 2:', )  #
+
+
+if __name__ == '__main__':
+    solver()
---
+++ 2020/day22/test_puzzle.py
@@ -0,0 +1,24 @@
+from .puzzle import parse, score, play_drunkard
+
+data = '''
+Player 1:
+9
+2
+6
+3
+1
+
+Player 2:
+5
+8
+4
+7
+10
+'''.strip()
+
+
+def test_score():
+    winner, winner_id = play_drunkard(*parse(data))
+    assert winner_id == 2
+    assert score(winner) == 306
+
