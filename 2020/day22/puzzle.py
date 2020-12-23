38488b11c2decbeb974e22371b8a6bb4bf04538a
Yuliya Bagriy <aviskase@gmail.com>
2020-12-22 20:41:16 -0500

    feat: day22 part 2

--- 2020/day22/puzzle.py
+++ 2020/day22/puzzle.py
@@ -28,6 +28,31 @@ def play_drunkard(player_1: THand, player_2: THand) -> (THand, int):
     return list(cur_2), 2
 
 
+def play_drunkard_recursive(player_1: THand, player_2: THand) -> (THand, int):
+    cur_1 = deque(player_1)
+    cur_2 = deque(player_2)
+    rounds = []
+    while len(cur_1) and len(cur_2):
+        if (list(cur_1), list(cur_2)) in rounds:
+            return list(cur_1), 1
+        rounds.append((list(cur_1), list(cur_2)))
+        card_1 = cur_1.popleft()
+        card_2 = cur_2.popleft()
+        if card_1 <= len(cur_1) and card_2 <= len(cur_2):
+            _, winner = play_drunkard_recursive(list(cur_1)[:card_1], list(cur_2)[:card_2])
+        else:
+            winner = 1 if card_1 > card_2 else 2
+        if winner == 1:
+            cur_1.append(card_1)
+            cur_1.append(card_2)
+        else:
+            cur_2.append(card_2)
+            cur_2.append(card_1)
+    if len(cur_1):
+        return list(cur_1), 1
+    return list(cur_2), 2
+
+
 def score(player: THand) -> int:
     return sum(i * c for i, c in enumerate(reversed(player), 1))
 
@@ -37,7 +62,8 @@ def solver():
         p1, p2 = parse(f.read().strip())
         winner_hand, winner_id = play_drunkard(p1, p2)
         print('Part 1:', score(winner_hand))  # 33403
-        # print('Part 2:', )  #
+        winner_hand, winner_id = play_drunkard_recursive(p1, p2)
+        print('Part 2:', score(winner_hand))  # 29177
 
 
 if __name__ == '__main__':
--- 2020/day22/test_puzzle.py
+++ 2020/day22/test_puzzle.py
@@ -1,4 +1,4 @@
-from .puzzle import parse, score, play_drunkard
+from .puzzle import parse, score, play_drunkard, play_drunkard_recursive
 
 data = '''
 Player 1:
@@ -22,3 +22,8 @@ def test_score():
     assert winner_id == 2
     assert score(winner) == 306
 
+
+def test_rec_score():
+    winner, winner_id = play_drunkard_recursive(*parse(data))
+    assert winner_id == 2
+    assert score(winner) == 291
