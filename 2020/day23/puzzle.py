0abbbcea2463921e8a443386c2402262066726f8
Yuliya Bagriy <aviskase@gmail.com>
2020-12-23 16:09:05 -0500

    feat: day23 part 2

--- 2020/day23/puzzle.py
+++ 2020/day23/puzzle.py
@@ -1,7 +1,8 @@
 from __future__ import annotations
 
 from dataclasses import dataclass
-from typing import Optional
+from operator import mul
+from typing import Optional, List
 
 
 @dataclass
@@ -11,18 +12,20 @@ class Cup:
 
 
 class Cups:
-    def __init__(self, data):
+    def __init__(self, data: List[int]):
         self.head = Cup(None)
         self.head.next = self.head
         current = self.head
+        self.index = {}
         for d in data:
             cup = Cup(d)
-            self.insert_after(current, cup)
-            current = cup
+            current = self.insert_after(current, cup)
+            self.index[d] = current
 
     def insert_after(self, after_cup: Cup, inserted_cup: Cup) -> Cup:
         inserted_cup.next = after_cup.next
         after_cup.next = inserted_cup
+        self.index[inserted_cup.data] = inserted_cup
         return inserted_cup
 
     def remove_after(self, after_cup: Cup) -> Cup:
@@ -32,6 +35,7 @@ class Cups:
         else:
             cup = after_cup.next
             after_cup.next = cup.next
+        del self.index[cup.data]
         return Cup(cup.data)
 
     def __iter__(self):
@@ -50,31 +54,20 @@ class Cups:
         return data
 
     def find(self, data):
-        current = self.head.next
-        while current != self.head:
-            if current.data == data:
-                return current
-            current = current.next
-        return None
+        return self.index.get(data, None)
 
     def find_destination_cup(self, destination):
         while destination >= 0:
-            cup = self.find(destination)
-            if cup:
-                return cup
+            if destination in self.index:
+                return self.index[destination]
             destination -= 1
 
-        current = self.head.next
-        max_cup = current
-        while current != self.head:
-            if current.data > max_cup.data:
-                max_cup = current
-            current = current.next
-        return max_cup
+        max_cup = max(self.index)
+        return self.index[max_cup]
 
 
 def play_game(data: str, moves: int) -> str:
-    cups = Cups(int(d) for d in data)
+    cups = Cups([int(d) for d in data])
     it = iter(cups)
     for move in range(moves):
         current = next(it)
@@ -90,11 +83,31 @@ def play_game(data: str, moves: int) -> str:
     return ''.join(str(x) for x in result[cup_1+1:] + result[:cup_1])
 
 
+def play_game2(data) -> (int, int):
+    start_data = [int(d) for d in data]
+    max_cup = max(start_data)
+    additional_data = [x for x in range(max_cup+1, 1000001)]
+    cups = Cups([int(d) for d in start_data+additional_data])
+    it = iter(cups)
+    for move in range(10000000):
+        current = next(it)
+        a = cups.remove_after(current)
+        b = cups.remove_after(current)
+        c = cups.remove_after(current)
+        destination = cups.find_destination_cup(current.data - 1)
+        a = cups.insert_after(destination, a)
+        b = cups.insert_after(a, b)
+        cups.insert_after(b, c)
+    cup_1 = cups.find(1)
+    a = cup_1.next
+    return a.data, a.next.data
+
+
 def solver():
     with open('input.txt', 'r') as f:
         data = f.read().strip()
         print('Part 1:', play_game(data, 100))  # 34952786
-        # print('Part 2:', )  #
+        print('Part 2:', mul(*play_game2(data)))  # 505334281774
 
 
 if __name__ == '__main__':
--- 2020/day23/test_puzzle.py
+++ 2020/day23/test_puzzle.py
@@ -1,9 +1,14 @@
 import pytest
 
-from .puzzle import play_game
+from .puzzle import play_game, play_game2
 
 
 @pytest.mark.parametrize('moves, expected', [(10, '92658374'), (100, '67384529')])
 def test_play_game(moves, expected):
     data = '389125467'
     assert play_game(data, moves) == expected
+
+
+def test_play_game2():
+    data = '389125467'
+    assert play_game2(data) == (934001, 159792)
